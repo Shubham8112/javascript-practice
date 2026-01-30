1895adb3b22846cdf5a7a9d69a44c1f0000f6b62
Shubham Pandey <shubpandeyham@gmail.com>
2026-01-30 16:14:14 +0530

    Javascript events learning

--- Lecture/lec9.js
+++ Lecture/lec9.js
@@ -18,3 +18,53 @@ let name = "Shubham", age = 22;
 console.log("My name is " + name + " and my age is " + age); // concatenation
 console.log(`My name is ${name} and my age is ${age}`); //template literal
 console.log(str.charAt(4)); // it return char at index 0
+/////////////
+console.log("javascript file loaded successfully");
+let num1 = 10;
+let num2 = 5;
+console.log("Sum:", num1 + num2);
+//conditional example
+if(num1 > num2){
+    console.log(`${num1} is greater than ${num2}`);
+}else{
+    console.log(`${num2} is greater than ${num1}`);
+}
+//for loop example
+for(let i = 0;i<num2;i++){
+    console.log(i);
+}
+//array example
+let arr1= [2,4,6,8,10];
+// arr1.forEach(element => {
+//     console.log("Element:", element);
+// });
+console.log(arr1);
+//push example:- push new entry at the end
+arr1.push(12);
+console.log("After push:", arr1);
+//pop example:- remove last entry
+arr1.pop();
+console.log("After pop:", arr1);
+//shift:- remove first entry
+arr1.shift();
+console.log("After shift:", arr1);
+//unshift:- add new entry at the start
+arr1.unshift(0);
+console.log("After unshift:", arr1);
+//object in javascript: key-value pair
+const student = {
+    name: "Ram",
+    age: 21,
+    marks:80
+}
+console.log(`Name: ${student.name},Age: ${student.age}, Marks: ${student.marks}`);
+//
+const students = [
+    {Name: "Shubham", Age:22, Marks:79},
+    {Name: "Ravi" ,Age:20, Marks:85},
+    {Name: "Ankit" ,Age:23, Marks:90}
+];
+for(let i=0;i<students.length;i++){
+    console.log(`Name: ${students[i].Name}, Age: ${students[i].Age}, Marks: ${students[i].Marks}`);
+}
+// ...........
--- events/script.js
+++ events/script.js
@@ -2,23 +2,22 @@ let count = 0;
 const p = document.querySelector('#counter');
 const addbtn = document.querySelector('#addbtn');
 const subtractbtn = document.querySelector('#subtractbtn');
-addbtn.addEventListener('click',()=>{
+addbtn.addEventListener('click',()=>{ //count + event
     // alert("clicked")
     count += 1;
     p.innerText = `Count: ${count}`;
 })
-subtractbtn.addEventListener('click',()=>{
+const p2 = document.createElement('p')          
+subtractbtn.addEventListener('click',()=>{ //count -ve event
     if(count>0){
         count -= 1;
         p.innerText = `Count :${count}`;
     }else if(count<1){
         p.innerText = `You cannot decrease less than 0`;
+        document.body.append(p2);
         p.style.color = 'rgb(123, 81, 27)';         
     }
 })
-const p2 = document.createElement('p')          
-p2.innerText = 'Hello this is a paragraph';
-document.body.append(p2);
 //button
 const btn = document.createElement("button");
 btn.innerText = "Reset";
@@ -27,4 +26,16 @@ document.body.append(btn);
 btn.addEventListener('click',()=>{
     count = 0;
     p.innerText = `Count: ${count}`; 
+})
+addbtn.addEventListener('mouseover',()=>{
+    addbtn.style.background = 'rgb(220, 142, 40)';
+})
+addbtn.addEventListener('mouseout',()=>{
+    addbtn.style.background = 'rgb(114, 46, 59)';
+})
+subtractbtn.addEventListener('mouseover',()=>{
+    subtractbtn.style.background = 'green';
+})
+subtractbtn.addEventListener('mouseout',()=>{
+    subtractbtn.style.background = 'rgb(56, 32, 91)';
 })
